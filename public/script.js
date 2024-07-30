924c40a6e7851df0f57958e6daf7a593883dfc5d
Sergio Veloza <velozasergio@gmail.com>
2024-07-29 20:12:56 -0500

    0.0.3

--- public/script.js
+++ public/script.js
@@ -9,44 +9,91 @@ $(document).ready(function () {
         });
 
         nanogallery2.nanogallery2({
-            "thumbnailHeight": 200,
-            "thumbnailWidth": "auto",
-            "thumbnailAlignment": "scaled",
-            "galleryMosaic": [                       // Opcional - Define el layout de la galería
-                { w: 1, h: 1, c: 1, r: 1 },
-                { w: 2, h: 1, c: 2, r: 1 },
-                { w: 1, h: 2, c: 1, r: 2 },
+            galleryMosaic: [                       
+                { w: 2, h: 2, c: 1, r: 1 },
                 { w: 1, h: 1, c: 3, r: 1 },
-                { w: 1, h: 1, c: 4, r: 1 },
                 { w: 1, h: 1, c: 3, r: 2 },
-                { w: 1, h: 1, c: 4, r: 2 },
-                { w: 1, h: 2, c: 5, r: 1 }
+                { w: 1, h: 2, c: 4, r: 1 },
+                { w: 2, h: 1, c: 5, r: 1 },
+                { w: 2, h: 2, c: 5, r: 2 },
+                { w: 1, h: 1, c: 4, r: 3 },
+                { w: 2, h: 1, c: 2, r: 3 },
+                { w: 1, h: 2, c: 1, r: 3 },
+                { w: 1, h: 1, c: 2, r: 4 },
+                { w: 2, h: 1, c: 3, r: 4 },
+                { w: 1, h: 1, c: 5, r: 4 },
+                { w: 1, h: 1, c: 6, r: 4 }
             ],
-            "thumbnailGutterWidth": 2,
-            "thumbnailGutterHeight": 2,
-            "thumbnailL1": 1,                       // Define el nivel de zoom en las miniaturas
-            "thumbnailL2": 1.2,
-            "thumbnailL3": 1.4
+            galleryMosaicXS: [                     
+                { w: 2, h: 2, c: 1, r: 1 },
+                { w: 1, h: 1, c: 3, r: 1 },
+                { w: 1, h: 1, c: 3, r: 2 },
+                { w: 1, h: 2, c: 1, r: 3 },
+                { w: 2, h: 1, c: 2, r: 3 },
+                { w: 1, h: 1, c: 2, r: 4 },
+                { w: 1, h: 1, c: 3, r: 4 }
+            ],
+            galleryMosaicSM: [                     
+                { w: 2, h: 2, c: 1, r: 1 },
+                { w: 1, h: 1, c: 3, r: 1 },
+                { w: 1, h: 1, c: 3, r: 2 },
+                { w: 1, h: 2, c: 1, r: 3 },
+                { w: 2, h: 1, c: 2, r: 3 },
+                { w: 1, h: 1, c: 2, r: 4 },
+                { w: 1, h: 1, c: 3, r: 4 }
+            ],
+            galleryMaxRows: 1,
+            galleryDisplayMode: 'rows',
+            gallerySorting: 'random',
+            thumbnailDisplayOrder: 'random',
+            thumbnailHeight: '180',
+            thumbnailWidth: '220',
+            thumbnailAlignment: 'scaled',
+            thumbnailGutterWidth: 0,
+            thumbnailGutterHeight: 0,
+            thumbnailBorderHorizontal: 0,
+            thumbnailBorderVertical: 0,
+            thumbnailToolbarImage: null,
+            thumbnailToolbarAlbum: null,
+            thumbnailLabel: { display: false },
+            galleryDisplayTransitionDuration: 1500,
+            thumbnailDisplayTransition: 'imageSlideUp',
+            thumbnailDisplayTransitionDuration: 1200,
+            thumbnailDisplayTransitionEasing: 'easeInOutQuint',
+            thumbnailDisplayInterval: 60,
+            thumbnailBuildInit2: 'image_scale_1.15',
+            thumbnailHoverEffect2: 'thumbnail_scale_1.00_1.05_300|image_scale_1.15_1.00',
+            touchAnimation: true,
+            touchAutoOpenDelay: 500,
+            viewerToolbar: { display: false },
+            viewerTools: {
+                topLeft: 'label',
+                topRight: 'shareButton, rotateLeft, rotateRight, fullscreenButton, closeButton'
+            },
+            galleryTheme: {
+                thumbnail: { background: '#111' },
+            },
+            locationHash: true
         });
     });
-});
 
-document.getElementById('uploadForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    
-    const formData = new FormData();
-    const fileField = document.getElementById('imageInput');
+    document.getElementById('imageInput').addEventListener('change', async (e) => {
+        e.preventDefault();
+        
+        const formData = new FormData();
+        const fileField = document.getElementById('imageInput');
 
-    formData.append('image', fileField.files[0]);
+        formData.append('image', fileField.files[0]);
 
-    try {
-        const response = await fetch('/upload', {
-            method: 'POST',
-            body: formData
-        });
-        alert('Imagen subida con éxito');
-        location.reload(); // Recargar la página para mostrar la nueva imagen
-    } catch (error) {
-        console.error('Error al subir la imagen:', error);
-    }
+        try {
+            const response = await fetch('/upload', {
+                method: 'POST',
+                body: formData
+            });
+            alert('Imagen subida con éxito');
+            location.reload(); // Recargar la página para mostrar la nueva imagen
+        } catch (error) {
+            console.error('Error al subir la imagen:', error);
+        }
+    });
 });
